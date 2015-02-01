4cc78e18a58fd77e01f80492aa84a6b412d14f76
Anti-Positron <poststrata@gmail.com>
2015-02-01 00:30:34 -0500

    Work and skills append functions
    added function to append work experience, added if statement to append
    skills, fixed syntax error in index.html, added to work object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -26,12 +26,23 @@ var bio = {
 };
 
 var work = {
-	"employer" : "T-Mobile",
-	"jobPosition" : "Retail Sales Associate",
-	"years" : "1",
-	"city" : "corona, NY"
+	"jobs" : [
+		{
+		"employer" : "T-Mobile",
+		"title" : "Retail Sales Associate",
+		"dates" : "06/13 - 04/14",
+		"location" : "Corona, NY",
+		"description" : "Greeted customers and assisted them as soon as they entered the location, Diagnosed customer’s lifestyle necessities and recommended a complete product solution, Amiably assisted customers in resolving issues concerning their T-Mobile account and devices, Assisted customers in purchasing new devices via T-Mobile iPad during busy hours, Handled cash/credit transactions and opened/closed point of sale cash registers"
+		},
+		{
+		"employer" : "Best Buy",
+		"title" : "Wireless Sales Lead",
+		"dates" : "12/12 - 06/13",
+		"location" : "Elmhurst, NY",
+		"description" : "Supervised and managed employee interaction with customers, Ensured sales floor is presentable and prices were updated, Handled inventory adjustments, received products and transferred products from other locations, Filled out daily management paperwork, Logged daily safe/ register totals and ordered change when needed, Was responsible for all tasks related to opening and closing the store"
+		}
+	]
 }
-
 
 var education = {
 	"schools" : [
@@ -93,3 +104,28 @@ var projects = {
 		"URL" : "github link"
 	}
 }
+//------------------------ CODE SECTION ---------------------------
+
+if (bio.skills.length >  0){
+	$('#header').append(HTMLskillsStart);
+
+	for(skill in bio.skills){
+		$('#skills').append(HTMLskills.replace( "%data%" , bio.skills[skill]));
+	}
+}
+var displayWork = function(){
+	for(job in work.jobs){
+		$('#workExperience').append(HTMLworkStart);
+		var formattedEmployer = HTMLworkEmployer.replace("%data%" , work.jobs[job].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%" , work.jobs[job].title);
+		var formattedEmployerTitle = formattedEmployer + formattedTitle;
+		$('.work-entry:last').append(formattedEmployerTitle);
+		var formattedDates = HTMLworkDates.replace("%data%" , work.jobs[job].dates);
+		$('.work-entry:last').append(formattedDates);
+		var formattedLocation = HTMLworkLocation.replace("%data%" , work.jobs[job].location);
+		$('.work-entry:last').append(formattedLocation);
+		var formattedDescription = HTMLworkDescription.replace("%data%" , work.jobs[job].description);
+		$('.work-entry:last').append(formattedDescription);	
+	}
+}
+displayWork();
